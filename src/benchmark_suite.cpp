86f9d5b670c57377092a5a01f348f8a56b6af70f
Leon <32157576+1p4pk@users.noreply.github.com>
2020-09-16 16:43:08 +0200

    Feature/benchmark suite (#12)
    * First version for benchmark suite.
    
    * New path to config file on remote server.

--- src/benchmark_suite.cpp
+++ src/benchmark_suite.cpp
@@ -1,3 +1,25 @@
 #include "benchmark_suite.hpp"
 
-namespace perma {}  // namespace perma
+#include "benchmark_factory.hpp"
+
+namespace perma {
+
+void BenchmarkSuite::start_benchmarks(const std::string& file_name) {
+  prepare_benchmarks(file_name);
+  run_benchmarks();
+}
+
+void BenchmarkSuite::prepare_benchmarks(const std::string& file_name) {
+  benchmarks_ = BenchmarkFactory::create_benchmarks(file_name);
+  for (std::unique_ptr<Benchmark>& benchmark : benchmarks_) {
+    benchmark->SetUp();
+  }
+}
+
+void BenchmarkSuite::run_benchmarks() {
+  for (std::unique_ptr<Benchmark>& benchmark : benchmarks_) {
+    benchmark->run();
+  }
+}
+
+}  // namespace perma
