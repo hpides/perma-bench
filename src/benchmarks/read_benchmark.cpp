711336b5dfbb42e4752e2c21a709cb5ac3742d0b
1p4pk <leonpapke@gmx.de>
2020-10-02 14:44:13 +0200

    Fix bug concerning created io_operations.

--- src/benchmarks/read_benchmark.cpp
+++ src/benchmarks/read_benchmark.cpp
@@ -11,16 +11,15 @@ void ReadBenchmark::get_result() {}
 void ReadBenchmark::set_up() {
   char* end_addr = pmem_file_ + get_length();
   // Create IOReadOperations
-  for (uint32_t i = 0; i < config_.number_operations_; i += internal::NUMBER_IO_OPERATIONS) {
+  for (uint32_t i = 1; i <= config_.number_operations_; i += internal::NUMBER_IO_OPERATIONS) {
+    io_operations_.push_back(std::make_unique<Read>(pmem_file_, end_addr, internal::NUMBER_IO_OPERATIONS,
+                                                    config_.access_size_, config_.exec_mode_));
     // Assumption: num_ops is multiple of internal::number_ios(1000)
-    if (i > 0 && i % config_.pause_frequency_ == 0) {
+    if (i % config_.pause_frequency_ == 0) {
       // Assumption: pause_frequency is multiple of internal:: number_ios (1000)
       auto pause_io = std::make_unique<Pause>(config_.pause_length_);
       io_operations_.push_back(std::move(pause_io));
     }
-
-    io_operations_.push_back(std::make_unique<Read>(pmem_file_, end_addr, internal::NUMBER_IO_OPERATIONS,
-                                                    config_.access_size_, config_.exec_mode_));
   }
 }
 
