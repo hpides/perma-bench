8fb4e53a6a5b6aade57911e4b017b3b9a9cdbf09
Lawrence Benson <github@lawben.com>
2020-11-09 10:57:25 +0100

    Use default config instead of global path (#27)
    * Use default config instead of global path
    
    * Update src/perma.cpp
    
    Co-authored-by: Leon <32157576+1p4pk@users.noreply.github.com>

--- src/perma.cpp
+++ src/perma.cpp
@@ -1,10 +1,17 @@
 #include "benchmark_suite.hpp"
 
+
 using namespace perma;
 
-int main() {
+constexpr auto DEFAULT_CONFIG_PATH = "configs/dev-config.yaml";
+
+int main(int argc, char** argv) {
+  std::filesystem::path config_file = std::filesystem::current_path() / ".." / DEFAULT_CONFIG_PATH;
+  if (argc == 2) {
+    config_file = argv[1];
+  }
   BenchmarkSuite bm_suite{};
-  bm_suite.run_benchmarks("/mnt/nvram-nvmbm/benchmark_config.yaml");
+  bm_suite.run_benchmarks(config_file);
 
   return 0;
 }
