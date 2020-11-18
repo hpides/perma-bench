d46d60cf6e38ae75352b4d7e7d69639e5da163b5
Lawrence <github@lawben.com>
2020-11-18 12:30:58 +0100

    Fix googletest

--- test/utils_test.cpp
+++ test/utils_test.cpp
@@ -5,5 +5,5 @@
 TEST(sample_test_case, sample_test) {
   const uint64_t value = perma::zipf(0.99, 1000);
   const bool is_between = 0 <= value && value < 1000;
-  EXPECT_EQ(true, is_between);
+  EXPECT_TRUE(is_between);
 }
