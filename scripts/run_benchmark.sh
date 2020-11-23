847cd7a99193f381cf74f2f9788710284e0091a5
Nina510 <ninaihde@ymail.com>
2020-11-23 21:07:52 +0100

    Add pmem directory

--- scripts/run_benchmark.sh
+++ scripts/run_benchmark.sh
@@ -3,6 +3,7 @@
 ############################################## DEFINE AND SET VARIABLES ################################################
 VISUALIZE=0
 DEBUG=0
+PMEM_DIR=/mnt/pmem
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
 for arg in "$@"
@@ -17,19 +18,21 @@ do
         DEBUG=1
         shift # Remove --debug from processing
         ;;
+        --server)
+        # shellcheck disable=SC2034
+        PMEM_DIR=/mnt/nvram-nvmbm
+        shift # Remove --server from processing
+        ;;
     esac
 done
 
 ############################################# BUILD AND RUN CMAKE PROJECT ##############################################
-# shellcheck disable=SC2164
-cd "$DIR/.."
-
 if [ "$DEBUG" == 1 ]; then
   cmake -DCMAKE_BUILD_TYPE=Debug -DCMAKE_MAKE_PROGRAM=/opt/rh/devtoolset-9/root/usr/bin/make -DCMAKE_C_COMPILER=/opt/rh/devtoolset-9/root/usr/bin/gcc -DCMAKE_CXX_COMPILER=/opt/rh/devtoolset-9/root/usr/bin/g++ -G "CodeBlocks - Unix Makefiles"
-  ./cmake-build-debug/src/perma-bench /mnt/nvram-nvmbm
+  "$DIR/../cmake-build-debug/src/perma-bench" PMEM_DIR
 else
   cmake -DCMAKE_BUILD_TYPE=Release -DCMAKE_MAKE_PROGRAM=/opt/rh/devtoolset-9/root/usr/bin/make -DCMAKE_C_COMPILER=/opt/rh/devtoolset-9/root/usr/bin/gcc -DCMAKE_CXX_COMPILER=/opt/rh/devtoolset-9/root/usr/bin/g++ -G "CodeBlocks - Unix Makefiles"
-  ./cmake-build-release/src/perma-bench /mnt/nvram-nvmbm
+  "$DIR/../cmake-build-release/src/perma-bench" PMEM_DIR
 fi
 
 ################################# SETUP VIRTUAL ENVIRONMENT AND EXECUTE VISUALIZATION ##################################
@@ -38,6 +41,7 @@ if [ "$VISUALIZE" == 1 ]; then
     mkdir "$DIR/../venv"
   fi
   python3 -m virtualenv "$DIR/../venv"
+
   # shellcheck disable=SC1090
   source "$DIR/../venv/bin/activate"
   pip3 install -r "$DIR/../requirements.txt"
