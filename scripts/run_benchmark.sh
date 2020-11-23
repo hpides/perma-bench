c38667aaf481fd58080d46a27c5bd9fb27409de9
Nina510 <ninaihde@ymail.com>
2020-11-23 19:22:51 +0100

    Add basic plot example; update cmake execution in run_benchmark script

--- scripts/run_benchmark.sh
+++ scripts/run_benchmark.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 
+############################################## DEFINE AND SET VARIABLES ################################################
 VISUALIZE=0
 DEBUG=0
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
@@ -14,28 +15,32 @@ do
         --debug)
         # shellcheck disable=SC2034
         DEBUG=1
-        shift # Remove --visualize from processing
+        shift # Remove --debug from processing
         ;;
     esac
 done
 
-
+############################################# BUILD AND RUN CMAKE PROJECT ##############################################
 # shellcheck disable=SC2164
 cd "$DIR/.."
-cmake -DCMAKE_BUILD_TYPE=Debug -DCMAKE_MAKE_PROGRAM=/opt/rh/devtoolset-9/root/usr/bin/make -DCMAKE_C_COMPILER=/opt/rh/devtoolset-9/root/usr/bin/gcc -DCMAKE_CXX_COMPILER=/opt/rh/devtoolset-9/root/usr/bin/g++ -G "CodeBlocks - Unix Makefiles"
 
 if [ "$DEBUG" == 1 ]; then
-  ./cmake-build-debug/src/perma-bench
+  cmake -DCMAKE_BUILD_TYPE=Debug -DCMAKE_MAKE_PROGRAM=/opt/rh/devtoolset-9/root/usr/bin/make -DCMAKE_C_COMPILER=/opt/rh/devtoolset-9/root/usr/bin/gcc -DCMAKE_CXX_COMPILER=/opt/rh/devtoolset-9/root/usr/bin/g++ -G "CodeBlocks - Unix Makefiles"
+  ./cmake-build-debug/src/perma-bench /mnt/nvram-nvmbm
 else
-  ./cmake-build-release/src/perma-bench
+  cmake -DCMAKE_BUILD_TYPE=Release -DCMAKE_MAKE_PROGRAM=/opt/rh/devtoolset-9/root/usr/bin/make -DCMAKE_C_COMPILER=/opt/rh/devtoolset-9/root/usr/bin/gcc -DCMAKE_CXX_COMPILER=/opt/rh/devtoolset-9/root/usr/bin/g++ -G "CodeBlocks - Unix Makefiles"
+  ./cmake-build-release/src/perma-bench /mnt/nvram-nvmbm
 fi
 
+################################# SETUP VIRTUAL ENVIRONMENT AND EXECUTE VISUALIZATION ##################################
 if [ "$VISUALIZE" == 1 ]; then
-  mkdir "$DIR/../venv"
+  if [ ! -d "$DIR/../venv" ]; then
+    mkdir "$DIR/../venv"
+  fi
   python3 -m virtualenv "$DIR/../venv"
-  pip3 install --user -r "$DIR/../requirements.txt"
   # shellcheck disable=SC1090
   source "$DIR/../venv/bin/activate"
+  pip3 install -r "$DIR/../requirements.txt"
 
   # shellcheck disable=SC2164
   python3 "$DIR/../visualization/main.py"
--- visualization/main.py
+++ visualization/main.py
@@ -1,7 +1,20 @@
 #! ../venv/bin/python
+import matplotlib.pyplot as plt
+import os
 
 
 def main():
+    # create img folder
+    viz_dir = os.path.dirname(__file__)
+    img_dir = os.path.join(viz_dir, "img/")
+
+    if not os.path.isdir(img_dir):
+        os.makedirs(img_dir)
+
+    # test plot
+    plt.plot([1, 2, 3, 4])
+    plt.savefig(img_dir + "/test.png")
+    plt.close()
     print("Visualization was successful.")
 
 
