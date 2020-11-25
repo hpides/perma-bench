0f2aa936d4c19e86c0ee1fe62faa0863cd62046f
Nina Ihde <ninaihde@ymail.com>
2020-11-25 13:15:47 +0100

    Add parameter for help and config path

--- scripts/run_benchmark.sh
+++ scripts/run_benchmark.sh
@@ -9,30 +9,46 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 for arg in "$@"
 do
     case $arg in
-        --visualize)
-        VISUALIZE=1
-        shift # Remove --visualize from processing
-        ;;
+        -h|--help)
+          echo "Welcome to PerMA-Bench"
+          echo " "
+          echo "The following options are available for executing PerMA-Bench:"
+          echo " "
+          echo "-h, --help                show brief help"
+          echo "-viz, --visualization     enable visualization"
+          echo "--debug                   use debug mode (default: release mode)"
+          echo "--server                  use pmem directory of server (default: pmem directory for docker)"
+          echo "--config=PATH             specify path to config file (default: configs/bm-suite.yaml)"
+          exit 0
+          ;;
+        -viz|--visualize)
+          VISUALIZE=1
+          shift # Remove --visualize from processing
+          ;;
         --debug)
-        # shellcheck disable=SC2034
-        DEBUG=1
-        shift # Remove --debug from processing
-        ;;
+          DEBUG=1
+          shift # Remove --debug from processing
+          ;;
         --server)
-        # shellcheck disable=SC2034
-        PMEM_DIR=/mnt/nvram-nvmbm
-        shift # Remove --server from processing
-        ;;
+          # shellcheck disable=SC2034
+          PMEM_DIR=/mnt/nvram-nvmbm
+          shift # Remove --server from processing
+          ;;
+        --config*)
+          # shellcheck disable=SC2155
+          export CONFIG_PATH=`echo $1 | sed -e 's/^[^=]*=//g'`
+          shift
+          ;;
     esac
 done
 
 ############################################# BUILD AND RUN CMAKE PROJECT ##############################################
 if [ "$DEBUG" == 1 ]; then
   cmake -DCMAKE_BUILD_TYPE=Debug -DCMAKE_MAKE_PROGRAM=/opt/rh/devtoolset-9/root/usr/bin/make -DCMAKE_C_COMPILER=/opt/rh/devtoolset-9/root/usr/bin/gcc -DCMAKE_CXX_COMPILER=/opt/rh/devtoolset-9/root/usr/bin/g++ -G "CodeBlocks - Unix Makefiles"
-  "$DIR/../cmake-build-debug/src/perma-bench" PMEM_DIR
+  "$DIR/../cmake-build-debug/src/perma-bench" PMEM_DIR CONFIG_PATH
 else
   cmake -DCMAKE_BUILD_TYPE=Release -DCMAKE_MAKE_PROGRAM=/opt/rh/devtoolset-9/root/usr/bin/make -DCMAKE_C_COMPILER=/opt/rh/devtoolset-9/root/usr/bin/gcc -DCMAKE_CXX_COMPILER=/opt/rh/devtoolset-9/root/usr/bin/g++ -G "CodeBlocks - Unix Makefiles"
-  "$DIR/../cmake-build-release/src/perma-bench" PMEM_DIR
+  "$DIR/../cmake-build-release/src/perma-bench" PMEM_DIR CONFIG_PATH
 fi
 
 ################################# SETUP VIRTUAL ENVIRONMENT AND EXECUTE VISUALIZATION ##################################
