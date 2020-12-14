558b01c66fe13325641a01b7a3b4e684fffdb1ac
Lawrence <github@lawben.com>
2020-12-14 12:31:35 +0100

    Get CI clock resolution

--- test/perma_test.cpp
+++ test/perma_test.cpp
@@ -1,7 +1,22 @@
 #include "gtest/gtest.h"
 
-int main(int argc, char** argv) {
-  ::testing::InitGoogleTest(&argc, argv);
+#include <chrono>
 
-  return RUN_ALL_TESTS();
+#include <iostream>
+#include <chrono>
+
+template <class Clock>
+void
+display_precision()
+{
+  typedef std::chrono::duration<double, std::nano> NS;
+  NS ns = typename Clock::duration(1);
+  std::cout << "PRECISION IN NS: " << ns.count() << " ns\n";
+}
+
+int main(int argc, char** argv) {
+  display_precision<std::chrono::high_resolution_clock>();
+//  ::testing::InitGoogleTest(&argc, argv);
+//
+//  return RUN_ALL_TESTS();
 }
