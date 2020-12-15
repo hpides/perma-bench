4f1a3901101d9026fc37d3584e959298b70671e0
1p4pk <leonpapke@gmx.de>
2020-12-15 13:36:50 +0100

    Add suggestions from call.

--- test/read_write_test.cpp
+++ test/read_write_test.cpp
@@ -21,15 +21,11 @@ class ReadWriteTest : public ::testing::Test {
   void SetUp() override {
     temp_file_ = generate_random_file_name(std::filesystem::temp_directory_path());
 
-    const std::string test_string = "test123456789";
-    std::ofstream temp_stream{temp_file_, std::ios::out};
-    temp_stream << test_string << std::endl;
-    temp_stream.flush();
+    std::ofstream temp_stream{temp_file_};
     temp_stream.close();
     std::filesystem::resize_file(temp_file_, TMP_FILE_SIZE);
-    ASSERT_EQ(std::filesystem::file_size(temp_file_), TMP_FILE_SIZE);
 
-    fd = open(temp_file_.c_str(), (O_CREAT | O_TRUNC | O_RDWR), S_IRWXU);
+    fd = open(temp_file_.c_str(), O_RDWR, S_IRWXU);
     ASSERT_GE(fd, 0);
 
     addr = static_cast<char*>(mmap(nullptr, TMP_FILE_SIZE, PROT_WRITE, MAP_SHARED, fd, 0));
