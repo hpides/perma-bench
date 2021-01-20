f1c40b4fe1330a4e8c628f07ab89314a8bd8439f
Nina Ihde <ninaihde@ymail.com>
2021-01-20 03:23:10 -0800

    Allow multiple matrix jsons

--- viz/png_creator.py
+++ viz/png_creator.py
@@ -28,43 +28,46 @@ class PngCreator:
                     plotter.latency_of_same_thread()
 
     def create_pngs_for_matrix_json(self):
-        # find json containing matrix arguments
-        matrix_json = glob.glob(self.results_dir + "*.json")
-        if len(matrix_json) != 1:
-            sys.exit("Please provide exactly one json file (containing matrix_args field).")
+        # collect jsons containing matrix arguments
+        matrix_jsons = list()
+        for path in glob.glob(self.results_dir + "*.json"):
+            matrix_jsons.append(path)
 
-        # init plotter
-        plotter = MatrixJsonPlotter(self.img_dir, matrix_json[0])
+        if len(matrix_jsons) < 1:
+            sys.exit(f"The visualization cannot be started until at least one JSON file is provided in {self.results_dir}.")
 
-        # iterate benchmark results
-        for bm_idx in range(plotter.reader.get_num_benchmarks()):
-            matrix_args = plotter.reader.get_result("matrix_args", bm_idx)
+        for mat_json in matrix_jsons:
+            plotter = MatrixJsonPlotter(self.img_dir, mat_json)
 
-            # prevent visualization of more than three matrix arguments
-            if len(matrix_args) > 3:
-                name = plotter.reader.get_result("bm_name", bm_idx)
-                print(f"Results of benchmark {name} cannot be visualized, because visualization of more than three "
-                      f"matrix arguments is not supported as there would be too many plots.")
-                continue
+            # iterate benchmark results
+            for bm_idx in range(plotter.reader.get_num_benchmarks()):
+                matrix_args = plotter.reader.get_result("matrix_args", bm_idx)
 
-            # create pngs for matrix with two or three dimensions
-            elif 2 <= len(matrix_args) <= 3:
-                perms = list(permutations(matrix_args, 2))
+                # prevent visualization of more than three matrix arguments
+                if len(matrix_args) > 3:
+                    name = plotter.reader.get_result("bm_name", bm_idx)
+                    print(f"Results of benchmark {name} cannot be visualized, because visualization of more than three "
+                          f"matrix arguments is not supported as there would be too many plots.")
+                    continue
 
-                for perm in perms:
-                    if perm[0] in plotter.reader.get_categorical_args():
-                        plotter.plot_categorical_x_and_legend(perm, "avg", bm_idx)
-                        plotter.plot_categorical_x_and_legend(perm, "bandwidth_values", bm_idx)
-                    else:
-                        plotter.plot_continuous_x_and_legend(perm, "avg", bm_idx)
-                        plotter.plot_continuous_x_and_legend(perm, "bandwidth_values", bm_idx)
+                # create pngs for matrix with two or three dimensions
+                elif 2 <= len(matrix_args) <= 3:
+                    perms = list(permutations(matrix_args, 2))
 
-            # create pngs for matrix with one dimension
-            else:
-                if matrix_args[0] in plotter.reader.get_categorical_args():
-                    plotter.plot_categorical_x(matrix_args[0], "avg", bm_idx)
-                    plotter.plot_categorical_x(matrix_args[0], "bandwidth_values", bm_idx)
+                    for perm in perms:
+                        if perm[0] in plotter.reader.get_categorical_args():
+                            plotter.plot_categorical_x_and_legend(perm, "avg", bm_idx)
+                            plotter.plot_categorical_x_and_legend(perm, "bandwidth_values", bm_idx)
+                        else:
+                            plotter.plot_continuous_x_and_legend(perm, "avg", bm_idx)
+                            plotter.plot_continuous_x_and_legend(perm, "bandwidth_values", bm_idx)
+
+                # create pngs for matrix with one dimension
                 else:
-                    pass
-                    plotter.plot_continuous_x(matrix_args[0], "avg", bm_idx)
-                    plotter.plot_continuous_x(matrix_args[0], "bandwidth_values", bm_idx)
+                    if matrix_args[0] in plotter.reader.get_categorical_args():
+                        plotter.plot_categorical_x(matrix_args[0], "avg", bm_idx)
+                        plotter.plot_categorical_x(matrix_args[0], "bandwidth_values", bm_idx)
+                    else:
+                        pass
+                        plotter.plot_continuous_x(matrix_args[0], "avg", bm_idx)
+                        plotter.plot_continuous_x(matrix_args[0], "bandwidth_values", bm_idx)
