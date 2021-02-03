fb49df85b762f5be03ed84c7ff3ed8a769e4d8c3
Nina Ihde <ninaihde@ymail.com>
2021-02-03 03:51:28 -0800

    Require venv instead of virtualenv

--- setup_viz.sh
+++ setup_viz.sh
@@ -4,7 +4,7 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
 if [ ! -d "$DIR/viz/venv" ]; then
     mkdir "$DIR/viz/venv"
-    python3 -m virtualenv "$DIR/viz/venv"
+    python3 -m venv "$DIR/viz/venv"
     source "$DIR/viz/venv/bin/activate"
     pip3 install -r "$DIR/viz/requirements.txt"
 fi
