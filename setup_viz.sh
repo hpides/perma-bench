2ac89f7e73ee64eb966fb93034792b342192efc2
Lawrence Benson <github@lawben.com>
2021-04-26 09:29:52 +0200

    Fix viz setup (#134)

--- setup_viz.sh
+++ setup_viz.sh
@@ -3,8 +3,8 @@
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
 if [ ! -d "$DIR/viz/venv" ]; then
-    mkdir "$DIR/viz/venv"
-    python3 -m venv "$DIR/viz/venv"
+    set -e
+    virtualenv "$DIR/viz/venv"
     source "$DIR/viz/venv/bin/activate"
     pip3 install -r "$DIR/viz/requirements.txt"
 else
