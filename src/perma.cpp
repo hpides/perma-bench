b8a21b8e32fad67368e4090618ff1da393a646b3
Lawrence Benson <github@lawben.com>
2021-04-30 17:34:44 +0200

    Move workloads to top-level dir (#139)
    * Move workloads to top-level dir

--- src/perma.cpp
+++ src/perma.cpp
@@ -7,7 +7,7 @@
 
 using namespace perma;
 
-constexpr auto DEFAULT_CONFIG_PATH = "configs/workloads";
+constexpr auto DEFAULT_WORKLOAD_PATH = "workloads";
 constexpr auto DEFAULT_RESULT_PATH = "results";
 
 int main(int argc, char** argv) {
@@ -20,9 +20,9 @@ int main(int argc, char** argv) {
   CLI::App app{"PerMA-Bench: Benchmark your Persistent Memory"};
 
   // Define command line args
-  std::filesystem::path config_file = std::filesystem::current_path() / DEFAULT_CONFIG_PATH;
+  std::filesystem::path config_file = std::filesystem::current_path() / DEFAULT_WORKLOAD_PATH;
   app.add_option("-c,--config", config_file,
-                 "Path to the benchmark config YAML file (default: " + std::string{DEFAULT_CONFIG_PATH} + ")")
+                 "Path to the benchmark config YAML file(s) (default: " + std::string{DEFAULT_WORKLOAD_PATH} + ")")
       ->check(CLI::ExistingPath);
 
   // Define result directory
