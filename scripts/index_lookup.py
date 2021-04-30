64aa9f72f31e715c2bc6c1cc84b7da337e63178c
Lawrence <github@lawben.com>
2021-04-30 18:46:02 +0200

    Merge index lookup plots

--- scripts/index_lookup.py
+++ scripts/index_lookup.py
@@ -3,29 +3,23 @@ import matplotlib.pyplot as plt
 
 from common import *
 
-def plot_bw(system_data, plot_dir):
-    fig, ax = plt.subplots(1, 1, figsize=SINGLE_FIG_SIZE)
-
-    bar_width = 0.8 / len(system_data)
+def plot_bw(system_data, ax):
+    bar_width = 0.8
     for i, (system, data) in enumerate(sorted(system_data.items())):
         _, y_data = zip(*data)
-        ax.bar(i, y_data, width=bar_width, label=SYSTEM_NAME[system], **BAR(system))
+        ax.bar(i, y_data, width=bar_width,
+               label=SYSTEM_NAME[system], **BAR(system))
 
-    x_ticks = [SYSTEM_NAME[s] for s in sorted(system_data.keys())]
-    ax.set_xticks(range(len(x_ticks)))
-    ax.set_xticklabels(x_ticks)
+    ax.set_xticks([])
     ax.set_ylabel("Bandwidth (GB/s)")
+    ax.set_ylim(0, 100)
 
     HATCH_WIDTH()
     Y_GRID(ax)
     HIDE_BORDERS(ax)
 
-    plot_path = os.path.join(plot_dir, "index_lookup_bw")
-    SAVE_PLOT(plot_path)
 
-def plot_lat(system_data_avg, system_data_99, plot_dir):
-    fig, ax = plt.subplots(1, 1, figsize=SINGLE_FIG_SIZE)
-
+def plot_lat(system_data_avg, system_data_99, ax):
     bars = sorted(system_data_avg.keys())
     num_bars = len(bars)
     bar_width = 0.8 / num_bars
@@ -38,9 +32,8 @@ def plot_lat(system_data_avg, system_data_99, plot_dir):
         color = SYSTEM_COLOR[system]
         hatch = SYSTEM_HATCH[system]
         bar = BAR(system)
-        ax.bar(0 + i * bar_width, y_data_avg, label=SYSTEM_NAME[system],
-               width=bar_width, **bar)
-        ax.bar(1 + i * bar_width, y_data_99, width=bar_width, **bar)
+        ax.bar(0 + i * bar_width, y_data_avg, width=bar_width, **bar)
+        ax.bar(1 + i * bar_width, y_data_99,  width=bar_width, **bar)
 
     x_ticks = ['AVG', '99%']
     x_ticks_pos = BAR_X_TICKS_POS(bar_width, num_bars, len(x_ticks))
@@ -48,15 +41,9 @@ def plot_lat(system_data_avg, system_data_99, plot_dir):
     ax.set_xticklabels(x_ticks)
     ax.set_ylabel("Latency (ns)")
 
-    fig.legend(loc='upper center', bbox_to_anchor=(0.5, 1.13), ncol=5,
-               frameon=False, columnspacing=1, handletextpad=0.3)
-
     HATCH_WIDTH()
     Y_GRID(ax)
     HIDE_BORDERS(ax)
-
-    plot_path = os.path.join(plot_dir, "index_lookup_lat")
-    SAVE_PLOT(plot_path)
 
 
 if __name__ == '__main__':
@@ -72,7 +59,15 @@ if __name__ == '__main__':
     lat_data_avg = get_data_from_runs(runs, "access_size", "duration", "avg")
     lat_data_99 = get_data_from_runs(runs, "access_size", "duration", "percentile_99")
 
-    plot_bw(bw_data, plot_dir)
-    plot_lat(lat_data_avg, lat_data_99, plot_dir)
 
+    fig, (bw_ax, lat_ax) = plt.subplots(1, 2, figsize=DOUBLE_FIG_SIZE)
+    plot_bw(bw_data, bw_ax)
+    plot_lat(lat_data_avg, lat_data_99, lat_ax)
+
+    fig.legend(loc='upper center', bbox_to_anchor=(0.5, 1.1), ncol=5,
+               frameon=False, columnspacing=1, handletextpad=0.3)
+    fig.tight_layout()
+
+    plot_path = os.path.join(plot_dir, "index_lookup")
+    SAVE_PLOT(plot_path)
     PRINT_PLOT_PATHS()
