f986113b0b9a3783461daa53403a964bcc0821af
1p4pk <leonpapke@gmx.de>
2021-05-18 12:10:33 +0200

    Added numa none and run on NVRAM03.

--- scripts/numa/numa.py
+++ scripts/numa/numa.py
@@ -1,18 +1,25 @@
+import sys
+import os
+sys.path.append(os.path.dirname(sys.path[0]))
+
 from common import *
 
 NUMA_COLOR = {
-    'far':  '#41b6c4',
-    'near':  '#378d54'
+    'far':  '#253494',
+    'near':  '#41b6c4',
+    'none': '#378d54'
 }
 
 NUMA_MARKER = {
     'far':  '^',
-    'near':  's'
+    'near':  's',
+    'none':  'o',
 }
 
 NUMA_PATTERN = {
     'far':  'Far',
-    'near':  'Near'
+    'near':  'Near',
+    'none':  'None',
 }
 
 
@@ -38,8 +45,8 @@ def plot_read(system_data, ax):
     ax.set_xlabel("Write Ratio")
 
     ax.set_ylabel("Bandwidth (GB/s)")
-    ax.set_ylim(0, 12)
-    ax.set_yticks(range(0, 15, 4))
+    ax.set_ylim(0, 21)
+    ax.set_yticks(range(0, 18, 6))
 
     ax.set_title("a) Read")
 
@@ -56,7 +63,7 @@ def plot_write(system_data, ax):
     ax.set_xticks([0, 0.2, 0.4, 0.6, 0.8, 1])
     ax.set_xlabel("Write Ratio")
 
-    ax.set_ylim(0, 3)
+    ax.set_ylim(0, 3.5)
     ax.set_yticks(range(0, 4, 1))
 
     ax.set_title("b) Write")
@@ -72,23 +79,33 @@ if __name__ == '__main__':
     filter_config_near = {
         "numa_pattern": "near"
     }
+    filter_config_none = {
+        "numa_pattern": "none"
+    }
 
     result_path, plot_dir = INIT(sys.argv)
 
     runs_far = get_runs_from_results(result_path, "index_update", filter_config_far)
     runs_near = get_runs_from_results(result_path, "index_update", filter_config_near)
+    runs_none = get_runs_from_results(result_path, "index_update", filter_config_none)
 
     bw_data_write_far = get_data_from_runs(runs_far, "write_ratio", "bandwidth", "write")
     bw_data_write_near = get_data_from_runs(runs_near, "write_ratio", "bandwidth", "write")
+    bw_data_write_none = get_data_from_runs(runs_none, "write_ratio", "bandwidth", "write")
     bw_data_read_far = get_data_from_runs(runs_far, "write_ratio", "bandwidth", "read")
     bw_data_read_near = get_data_from_runs(runs_near, "write_ratio", "bandwidth", "read")
+    bw_data_read_none = get_data_from_runs(runs_none, "write_ratio", "bandwidth", "read")
 
     bw_data_write = {}
     bw_data_read = {}
-    bw_data_write['near'] = list(bw_data_write_near.values())[0]
-    bw_data_read['near'] = list(bw_data_read_near.values())[0]
+    bw_data_none = {}
+
     bw_data_write['far'] = list(bw_data_write_far.values())[0]
     bw_data_read['far'] = list(bw_data_read_far.values())[0]
+    bw_data_write['near'] = list(bw_data_write_near.values())[0]
+    bw_data_read['near'] = list(bw_data_read_near.values())[0]
+    bw_data_write['none'] = list(bw_data_write_none.values())[0]
+    bw_data_read['none'] = list(bw_data_read_none.values())[0]
 
     fig, (read_ax, write_ax) = plt.subplots(1, 2, figsize=DOUBLE_FIG_SIZE)
 
