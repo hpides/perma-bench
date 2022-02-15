a5707f63ea4ded0bad90cf855601d81347dcdf7a
Lawrence Benson <github@lawben.com>
2022-02-15 19:21:39 +0100

    Add speed plots

--- scripts/speed/speed.py
+++ scripts/speed/speed.py
@@ -5,27 +5,27 @@ sys.path.append(os.path.dirname(sys.path[0]))
 from common import *
 
 SPEED_COLOR = {
-    "speed2133":      '#fecc5c',
-    "speed2400":      '#fd8d3c',
-    "speed2666":      '#f03b20',
-    "speed2133-dram": '#bd0026',
-    "speed2666-dram": '#bd0026',
+    "2133":      '#fecc5c',
+    "2400":      '#fd8d3c',
+    "2666":      '#f03b20',
+    "2133-dram": '#bd0026',
+    "2666-dram": '#bd0026',
 }
 
 SPEED_NAME = {
-    "speed2133": '2133',
-    "speed2400": '2400',
-    "speed2666": '2666',
-    "speed2133-dram": '2133-DRAM',
-    "speed2666-dram": '2666-DRAM',
+    "2133": '2133 MT/s',
+    "2400": '2400 MT/s',
+    "2666": '2666 MT/s',
+    "2133-dram": '2133-DRAM',
+    "2666-dram": '2666-DRAM',
 }
 
 SPEED_HATCH = {
-    "speed2133": '\\\\',
-    "speed2400": '//',
-    "speed2666": '\\',
-    "speed2133-dram": '//',
-    "speed2666-dram": 'x',
+    "2133": '\\\\',
+    "2400": '//',
+    "2666": '\\',
+    "2133-dram": '//',
+    "2666-dram": 'x',
 }
 
 def SPEED_BAR(system):
@@ -36,61 +36,103 @@ def SPEED_BAR(system):
         "lw": 3
     }
 
-def plot_dimms(scan_data, logging_data, index_data, ax):
-    bars = sorted(scan_data.keys())
+def plot_bm(system_data, ax, offset, label=False):
+    bars = sorted(system_data.keys())
     num_bars = len(bars)
     bar_width = 0.8 / num_bars
 
-    num_xticks = 3
+    num_xticks = num_bars
     x_pos = range(num_xticks)
 
-    def plot_bm(system_data, offset, label=False):
-        for i, system in enumerate(bars):
-            data = system_data[system]
-            y_data = data[-1][1]
-            # print(f"{system}: {y_data}")
-            pos = x_pos[offset] + (i * bar_width)
-            bar = ax.bar(pos, y_data, width=bar_width, **SPEED_BAR(system))
-            if label:
-                label = SPEED_NAME[system]
-                bar.set_label(label)
+    for i, system in enumerate(bars):
+        data = system_data[system]
+        y_data = data[-1][1]
+        if y_data > MILLION:
+            y_data = y_data / MILLION
+        # print(f"{system}: {y_data}")
+        pos = x_pos[offset] + (i * bar_width)
+        bar = ax.bar(pos, y_data, width=bar_width, **SPEED_BAR(system))
+        if label:
+            label = SPEED_NAME[system]
+            bar.set_label(label)
 
-    plot_bm(scan_data, 0, label=True)
-    plot_bm(logging_data, 1)
-    plot_bm(index_data, 2)
 
-    ax.set_xticks(BAR_X_TICKS_POS(bar_width, num_bars, num_xticks))
-    ax.set_xticklabels(["Table\nScan", "Log-\nging", "Index\nLookup"])
+def plot_raw(scan_data, logging_data, index_data, ax):
+    plot_bm(scan_data, ax, 0, label=True)
+    plot_bm(logging_data, ax, 1)
+    plot_bm(index_data, ax, 2)
 
-    ax.set_ylim(0, 45)
-    ax.set_yticks(range(0, 45, 10))
-
+    ax.set_xticks(BAR_X_TICKS_POS(0.8 / 3, 3, 3))
+    ax.set_xticklabels(["Seq.\nRead", "Seq.\nWrite", "Rnd.\nRead"])
     ax.set_ylabel("Throughput (GB/s)")
+    ax.set_ylim(0, 40)
+    ax.set_yticks(range(0, 41, 10))
+    ax.set_title("a) Raw")
+
+
+def plot_ops(hash_data, tree_data, ax):
+    plot_bm(hash_data, ax, 0)
+    plot_bm(tree_data, ax, 1)
+
+    ax.set_xticks(BAR_X_TICKS_POS(0.8 / 3, 3, 2))
+    ax.set_xticklabels(["Update\nHash", "Tree\nLookup"])
+    ax.set_ylabel("Million Ops/s")
+    ax.set_ylim(0, 30)
+    ax.set_yticks(range(0, 31, 5))
+    ax.set_title("b) Index")
+
+
+def plot_lat(latency_data, ax):
+    plot_bm(latency_data, ax, 0)
+    ax.set_xticks(BAR_X_TICKS_POS(0.8 / 3, 3, 1))
+    ax.set_xticklabels(["64 Byte\nRead"])
+    ax.set_ylabel("Latency (ns)")
+    ax.set_ylim(0, 550)
+    ax.set_yticks(range(0, 551, 100))
+    ax.set_title("c) Latency")
 
 
 if __name__ == '__main__':
     skip_dram = True
     result_path, plot_dir = INIT(sys.argv)
 
-    scan_config = {"access_size": 16384}
-    scan_runs = get_runs_from_results(result_path, "table_scan_partitioned", scan_config, skip_dram=skip_dram)
-    scan_data = get_data_from_runs(scan_runs, "number_threads", "bandwidth", "read")
+    scan_config = {"access_size": 4096}
+    scan_runs = get_runs_from_results(result_path, "sequential_reads", scan_config, skip_dram=skip_dram)
+    scan_data = get_data_from_runs(scan_runs, "number_threads", "bandwidth")
 
     logging_config = {"access_size": 512, "persist_instruction": "nocache"}
-    logging_runs = get_runs_from_results(result_path, "logging_partition", logging_config, skip_dram=skip_dram)
-    logging_data = get_data_from_runs(logging_runs, "number_partitions", "bandwidth", "write")
+    logging_runs = get_runs_from_results(result_path, "sequential_writes", logging_config, skip_dram=skip_dram)
+    logging_data = get_data_from_runs(logging_runs, "number_threads", "bandwidth")
 
-    index_config = {"access_size": 256, "random_distribution": "uniform"}
-    index_runs = get_runs_from_results(result_path, "index_lookup", index_config, skip_dram=skip_dram)
-    index_data = get_data_from_runs(index_runs, "number_partitions", "bandwidth", "read")
+    index_config = {"access_size": 256}
+    index_runs = get_runs_from_results(result_path, "random_reads", index_config, skip_dram=skip_dram)
+    index_data = get_data_from_runs(index_runs, "number_threads", "bandwidth")
 
-    fig, ax = plt.subplots(1, 1, figsize=SINGLE_FIG_SIZE)
-    plot_dimms(scan_data, logging_data, index_data, ax)
+    hash_index_config = {"custom_operations": "rp_512,wp_64_cache_128,wp_64_cache_-128"}
+    hash_index_runs = get_runs_from_results(result_path, "hash_index_update", hash_index_config, skip_dram=skip_dram)
+    hash_index_data = get_data_from_runs(hash_index_runs, "number_threads", "ops_per_second")
+
+    tree_index_config = {"custom_operations": "rp_512,rp_512,rp_512"}
+    tree_index_runs = get_runs_from_results(result_path, "tree_index_lookup", tree_index_config, skip_dram=skip_dram)
+    tree_index_data = get_data_from_runs(tree_index_runs, "number_threads", "ops_per_second")
+
+    latency_config = {"custom_operations": "rp_64"}
+    latency_runs = get_runs_from_results(result_path, "operation_latency", latency_config, skip_dram=skip_dram)
+    latency_data = get_data_from_runs(latency_runs, "number_threads", "latency", "avg")
+
+    fig, axes = plt.subplots(1, 3, figsize=DOUBLE_FIG_SIZE, gridspec_kw={'width_ratios': [3, 2, 1]})
+    raw_ax, ops_ax, lat_ax = axes
+
+    plot_raw(scan_data, logging_data, index_data, raw_ax)
+    plot_ops(hash_index_data, tree_index_data, ops_ax)
+    plot_lat(latency_data, lat_ax)
 
     HATCH_WIDTH()
     FIG_LEGEND(fig)
-    Y_GRID(ax)
-    HIDE_BORDERS(ax)
+
+    for ax in axes:
+        Y_GRID(ax)
+        HIDE_BORDERS(ax)
 
     plot_path = os.path.join(plot_dir, "speed_performance")
     SAVE_PLOT(plot_path)
